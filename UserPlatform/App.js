a4e57de9fc34bf8188eb755cf0eb20aa5fcbbeb5
SmaouiAli <smaouiali22@gmail.com>
2020-11-23 08:41:16 +0100

    cleaning app js code

--- UserPlatform/App.js
+++ UserPlatform/App.js
@@ -1,6 +1,5 @@
 import "react-native-gesture-handler";
 import React from "react";
-import { View } from "react-native";
 import Home from "./components/Home/Home.js";
 import SignIn from "./components/SignIn/SignIn.js";
 import SignUp from "./components/SignUp/SignUp.js";
@@ -11,7 +10,7 @@ import History from './components/History/History.js';
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import {Provider} from 'react-redux';
-import store from './store.js'
+import store from './store.js';
 const Stack = createStackNavigator();
 
 export default function App() {
@@ -32,4 +31,4 @@ export default function App() {
     </NavigationContainer>
 
   );
-}
+};
