57d1ec22e29be894c2417eefb0311c758fd1d74d
SmaouiAli <smaouiali22@gmail.com>
2020-11-26 11:44:08 +0100

    Updating User Profile Picture.

--- UserPlatform/Components/Uploadimage/Uploadimage.js
+++ UserPlatform/Components/Uploadimage/Uploadimage.js
@@ -1,93 +1,71 @@
-import React,{ Component, useState } from "react";
-import { View, Text,Image } from "react-native";
+import React, { Component, useState } from "react";
+import { View, Text, Image } from "react-native";
 import Icon from "react-native-vector-icons/FontAwesome";
-import { Input, Card, Button } from "react-native-elements";
+import { Input, Card, Button, Avatar } from "react-native-elements";
 import Axios from "axios";
-
-export default class  Uploadimage extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            image : {}
-        }
-        this.handleFileInputChange = this.handleFileInputChange.bind(this);
-        this.handleSubmitFile = this.handleSubmitFile.bind(this);
-    }
-    // const [fileInputState, setFileInputState] = useState('');
-    // const [previewSource, setPreviewSource] = useState({});
-    // const [selectedFile, setSelectedFile] = useState({});
-
-     handleFileInputChange(e){
-        const file = e.target.files[0];
-        const data = new FormData();
-        data.append('file', file);
-        data.append('upload_preset', 'angular_cloudinary');
-        data.append('cloud_name', 'codexmaker');
-        this.setState({image: data});
-        console.log(data);
-        console.log(file)
+import store from "../../store.js";
+export default class Uploadimage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      image: {},
+      status: "",
     };
-    // const previewFile = (file) => {
-    //     const reader = new FileReader();
-    //     reader.readAsDataURL(file);
-    //     reader.onloadend = () => {
-    //         setPreviewSource(reader.result);
-    //     };
-    // };
-    handleSubmitFile(e){
-    e.preventDefault();
-    //     if (!selectedFile) return;
-    //     const reader = new FileReader();
-    //     reader.readAsDataURL(selectedFile);
-    //     reader.onloadend = () => {
-    //         uploadImage(reader.result);
-    //     };
-    //     reader.onerror = () => {
-    //         console.error('AHHHHHHHH!!');
-            
-    //     };
-    // };
-    // const uploadImage = async (base64EncodedImage) => {
-    //     try {
-             Axios.post('https://api.cloudinary.com/v1_1/codexmaker/image/upload',this.state.image)
-            .then((res)=>{
-                console.log(res)
-            })
+    this.handleFileInputChange = this.handleFileInputChange.bind(this);
+    this.handleSubmitFile = this.handleSubmitFile.bind(this);
+  }
+  handleFileInputChange(e) {
+    const file = e.target.files[0];
+    const data = new FormData();
+    data.append("file", file);
+    data.append("upload_preset", "angular_cloudinary");
+    data.append("cloud_name", "codexmaker");
+    this.setState({ image: data });
+    console.log(data);
+    console.log(file);
+  }
 
-            // setFileInputState('');
-            // setPreviewSource('');
-           
-        // } catch (err) {
-        //     console.error(err);
-        // }
-        }
-    render(){
-  return (
-    <View>
-      <Card>
-      <form onSubmit={this.handleSubmitFile} className="form">
-                <input
-                    id="fileInput"
-                    type="file"
-                    name="image"
-                    onChange={this.handleFileInputChange}
-                    // value={fileInputState}
-                    className="form-input"
-                />
-                <button className="btn" type="submit">
-                    Submit
-                </button>
-            </form>
-      </Card>
-      
-      {/* {previewSource && (
-               <Card> <Image
-                    // src={previewSource}
-                    alt="chosen"
-                />
-                </Card>
-            )} */}
-    </View>
-  );
-}
+  handleSubmitFile(e) {
+    e.preventDefault();
+    const storedData = store.getState();
+    console.log(storedData.auth.phone);
+    Axios.post(
+      "https://api.cloudinary.com/v1_1/codexmaker/image/upload",
+      this.state.image
+    ).then((res) => {
+      console.log(res.data.url);
+      this.props.imageChange(res.data.url);
+      Axios.post("http://localhost:5000/users/update/image", {
+        image: res.data.url,
+        phone: storedData.auth.phone,
+      })
+        .then((res) => {
+          console.log("image updated!");
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    });
+    this.setState({ status: "shown" });
+  }
+  render() {
+    return (
+      <View>
+        <Card>
+          <form onSubmit={this.handleSubmitFile} className="form">
+            <input
+              id="fileInput"
+              type="file"
+              name="image"
+              onChange={this.handleFileInputChange}
+              className="form-input"
+            />
+            <button className="btn" type="submit">
+              Submit
+            </button>
+          </form>
+        </Card>
+      </View>
+    );
+  }
 }
